a306c4ba70e0cc653f4e1b63fea82186d757875c
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-04-24 15:29:46 -0300

    proyect init

---
+++ build/index.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("hola estoy utilizando ts");
+//esto es un documentario con barritas
+/*esto es un comentario multilinea*/
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("hola estoy utilizando ts");
+
+//esto es un documentario con barritas
+
+/*esto es un comentario multilinea*/
